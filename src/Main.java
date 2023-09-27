c031b98a42587cebd1847a491e5c057e40d4960b
pai-puella <cat.dyshkant@yandex.ru>
2023-09-27 14:32:27 +0300

    Added Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Задача 1");
+        int targetAmount = 2459000;
+        int monthlySaving = 15000;
+        double currentAmount = 0;
+        int months = 0;
+        double interest = 0.12;
 
+        while (currentAmount < targetAmount) {
+            currentAmount += monthlySaving;
+            currentAmount += currentAmount * interest / 12;
+            months++;
+        }
+
+        System.out.println("Месяц " + months + ", сумма накоплений равна " + currentAmount + " рублей");
+        System.out.println();
     }
 }
