8c1f995c5f45fba8935ccfbae09cfa237ed87d13
pai-puella <cat.dyshkant@yandex.ru>
2023-09-27 14:38:29 +0300

    Added Task 2

--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,21 @@ public class Main {
             currentAmount += currentAmount * interest / 12;
             months++;
         }
-
         System.out.println("Месяц " + months + ", сумма накоплений равна " + currentAmount + " рублей");
         System.out.println();
+
+        System.out.println("Задача 2");
+        int num = 1;
+        while (num <= 10) {
+            System.out.print(num + " ");
+            num++;
+        }
+        System.out.println();
+        for (int i = 10; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+
     }
 }
